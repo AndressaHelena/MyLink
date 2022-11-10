3740d55b0ff992120bb2ed9ec90b6e83df9bef80
Andressa Helena <andressa.h.moreira@avanade.com>
2022-11-10 09:18:21 -0300

    Arrumando Estilo

--- src/pages/Error/styles.js
+++ src/pages/Error/styles.js
@@ -1,28 +1,32 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-export const Container = styled.body`
-display: flex;
- flex-direction: column;
- width: 100%;
- min-width: 100vh;
- justify-content: center;
- align-items: center;
- background: black;
+export const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  max-width: 100%;
+  padding: 14px;
+  justify-content: center;
+  align-items: center;
+  background: black;
 `
 export const Title = styled.h2`
-color: red;
+  color: red;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 `
 export const BackLink = styled(Link)`
-background-color: rgba(255, 255, 255, 0.6);
-text-decoration: none;
-color: #555;
-padding: 4px 16px;
-margin-top: 14px;
-border-radius: 5px;
-transition: background-color 0.5s;
-cursor: pointer;
-&:hover {
+  background-color: rgba(255, 255, 255, 0.6);
+  text-decoration: none;
+  color: #555;
+  padding: 4px 16px;
+  margin-top: 14px;
+  border-radius: 5px;
+  transition: background-color 0.5s;
+  cursor: pointer;
+  &:hover {
   color: #333;
   background: #fff;
   transform: scale(1.05);
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,46 +1,46 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    padding: 14px 0;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 14px 0;
+  justify-content: center;
+  align-items: center;
 `
 export const Title = styled.h1`
-    margin-bottom: 14px;
-    color: #fff;
-    font-size: 40px;
-    padding-top: 94px;
-    color: #fff;
+  margin-bottom: 14px;
+  color: #fff;
+  font-size: 40px;
+  padding-top: 94px;
+  color: #fff;
 `
 export const Span = styled.span`
-    color: #f1f1f1;
-    margin-bottom: 18px;
+  color: #f1f1f1;
+  margin-bottom: 18px;
 `
 
 export const Links = styled.main`
-    display: flex;
-    flex-direction: column;
-    max-width: 600px;
-    width: 90%;
-    text-align: center;
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+  width: 90%;
+  text-align: center;
 `
 export const LinkArea = styled.section`
-    background-color: #fff;
-    width: 100%;
-    margin-bottom: 18px;
-    padding: 8px 0;
-    user-select: none;
-    border-radius: 5px;
-    transition: transform 0.4s;
-    :hover{
-        transform: scale(1.04);
-    }
+  background-color: #fff;
+  width: 100%;
+  margin-bottom: 18px;
+  padding: 8px 0;
+  user-select: none;
+  border-radius: 5px;
+  transition: transform 0.4s;
+  :hover{
+  transform: scale(1.04);
+  }
 `
 export const LinkText = styled.p`
-    font-size: 18px;
-    line-height: 150%;
-    color: var(--azul-escuro);
+  font-size: 18px;
+  line-height: 150%;
+  color: var(--azul-escuro);
 `
